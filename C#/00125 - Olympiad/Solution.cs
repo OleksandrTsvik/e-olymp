ffc449e407e048fc415f5b1d39b5f692c73fce31
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2024-11-11 00:59:48 +0200

    sol(csharp): init

---
+++ C#/00125 - Olympiad/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string[] st1 = Console.ReadLine().Split();
+        string[] st2 = Console.ReadLine().Split();
+
+        int h1 = int.Parse(st1[0]); int h2 = int.Parse(st2[0]);
+        int m1 = int.Parse(st1[1]); int m2 = int.Parse(st2[1]);
+        int s1 = int.Parse(st1[2]); int s2 = int.Parse(st2[2]);
+
+        s1 += m1 * 60 + h1 * 3600;
+        s2 += m2 * 60 + h2 * 3600;
+
+        int result = s2 - s1;
+        int h = result / 3600; result -= h * 3600;
+        int m = result / 60; result -= m * 60;
+        int s = result;
+
+        Console.WriteLine($"{h} {m} {s}");
+    }
+}
---
+++ C#/07846 - Maximum element/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];
+        int max = 0, index = 0;
+
+        string[] num = Console.ReadLine().Split(' ');
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = int.Parse(num[i]);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] > max)
+            {
+                max = a[i];
+                index = i + 1;
+            }
+        }
+
+        Console.WriteLine($"{max} {index}");
+    }
+}
